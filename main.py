96d4e3d5fae4cd524c3d67364f9ab47efde3c4ea
Alex-Golovko <ya.golovko-alex2017@yandex.ru>
2021-06-27 15:00:01 +0300

    rename superhoros

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from pprint import pprint
+
 import requests
+
 from yadisk import YandexDisk
 
-TOKEN = 'AQAAAAAWwSZPAADLW55Sy2uaJEbAlEL1Ms0UfJA'
+TOKEN = "AQAAAAAWwSZPAADLW55Sy2uaJEbAlEL1Ms0UfJA"
 
-ya = YandexDisk(token=TOKEN)
-ya.upload_file_to_disk('test/netology', 'test.txt')
+if __name__ == '__main__':
+    ya = YandexDisk(token="AQAAAAAWwSZPAADLW55Sy2uaJEbAlEL1Ms0UfJA")
+    ya.upload_file_to_disk("C:/Users/Саша-Оля/PycharmProjects/netology-requests", "test.txt")
