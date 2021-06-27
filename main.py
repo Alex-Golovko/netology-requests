c557f142464df797a06bcf373897b68f1ab88774
Alex-Golovko <ya.golovko-alex2017@yandex.ru>
2021-06-27 15:05:17 +0300

    delet prived TOKEN

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import requests
 
 from yadisk import YandexDisk
 
-TOKEN = "AQAAAAAWwSZPAADLW55Sy2uaJEbAlEL1Ms0UfJA"
+TOKEN = ""
 
 if __name__ == '__main__':
-    ya = YandexDisk(token="AQAAAAAWwSZPAADLW55Sy2uaJEbAlEL1Ms0UfJA")
+    ya = YandexDisk(token="")
     ya.upload_file_to_disk("C:/Users/Саша-Оля/PycharmProjects/netology-requests", "test.txt")
