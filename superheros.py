74d46dc9a153a10dcda5490f36b7f88a56525619
Alex-Golovko <ya.golovko-alex2017@yandex.ru>
2021-06-27 14:44:51 +0300

    rename superhoros

--- superheros.py
+++ superheros.py
@@ -1,8 +1,10 @@
-import superheros
-superhero_url = 'https://superheroapi.com/api/2619421814940190/search/name'
-superheroes = [{'name': 'Halk'}, {'name': 'Captain America'},{'name': 'Thanos'}]
+import requests
+
+namesearch_url = "https://superheroapi.com/api/2619421814940190/search/"
+superheroes = [{'name': 'Hulk'}, {'name': 'Captain America'}, {'name': 'Thanos'}]
 
 for hero in superheroes:
-    hero_r = superheros.get(superhero_url + hero['name'])
+    hero_r = requests.get(namesearch_url + hero['name'])
     hero['intelligence'] = int(hero_r.json()['results'][0]['powerstats']['intelligence'])
+
 print(sorted(superheroes, key=lambda hero: -hero['intelligence'])[0]['name'])
